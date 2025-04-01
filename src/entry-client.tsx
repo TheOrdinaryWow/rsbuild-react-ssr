2e65c796d4e4d0e4389c81f153262a4e7ca1f81b
TheOrdinaryWow <25114850+TheOrdinaryWow@users.noreply.github.com>
2025-04-01 15:58:43 +0800

    chore: remove unused code

--- src/entry-client.tsx
+++ src/entry-client.tsx
@@ -1,8 +1,6 @@
 import { StartClient } from "@tanstack/react-start";
-import React from "react";
 import ReactDOM from "react-dom/client";
 
-import { RouterProvider } from "@tanstack/react-router";
 import { createRouter } from "./router";
 
 const router = createRouter();
--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -1,3 +1,4 @@
+import { createMemoryHistory } from "@tanstack/react-router";
 import { StartServer } from "@tanstack/react-start/server";
 import React from "react";
 import {
@@ -7,12 +8,7 @@ import {
   renderToString as _renderToString,
 } from "react-dom/server";
 
-import { RouterProvider, createMemoryHistory } from "@tanstack/react-router";
 import { createRouter } from "./router";
-
-// const memoryHistory = createMemoryHistory({
-//   initialEntries: [opts.url],
-// });
 
 export async function renderToPipeableStream(url: string, options?: RenderToPipeableStreamOptions) {
   const router = createRouter();
